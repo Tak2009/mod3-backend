91f1ef98edee68f712acd8d86c52cbc52f66a58c
github username <github email address>
2019-10-19 17:25:46 +0100

    seed created

--- README.md
+++ README.md
@@ -27,4 +27,5 @@ Things you may want to cover:
 2. rails g resource portfolio local_amt home_amt exchange:references
 3. rails g resource transaction sell_amt buy_amt effectivefx portfolio:references
 4. establish associations: belngs_to/has_many
-
+5. rails db:migrate
+5. create seed data!S
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+#1. 以下はExchnageRateをゲットするためのもの
+require 'net/http'
+require 'uri'
+require 'json'
+
+uri = URI.parse('http://www.apilayer.net/api/live?access_key=d472b0aa4dbe0894022c08656f86a91e&currencies=USD,JPY,GBP,AUD&format=1')
+json = Net::HTTP.get(uri) #NET::HTTPを利用してAPIを呼ぶ
+result = JSON.parse(json) #返ってきたjsonデータをrubyの配列に変換するためのライン
+
+# 2.ここからはデータベースに取得したレートを保存するためのもの
+result['quotes'].each do |key, value|
+    if Exchange.find_by(currency: key)
+      rate = Exchange.find_by(currency: key)
+      rate.update(rate: value)
+    else
+      Exchange.create(currency: key, rate: value)
+    end
+  end
+
+# 3.GBPベースでないので
+ new_base_currency_gbp_rate = result['quotes']['USDGBP']
+ result_base_currency_gbp = result['quotes'].map{|key,value| [key, (value/new_base_currency_gbp_rate).round(4)]}.to_h #GBPをGBPで割ってしまうためUSDGBPレートが１に塗り変わってしまうが先頭のUSDUSDがGBPの元になる
+ result_base_currency_gbp_with_right_key_name = result_base_currency_gbp.map{|key,value| ["GBP" + key.slice(3,6), value]}.to_h # keynameの調整
+ 
+# 4.dbにセーブ
+ result_base_currency_gbp_with_right_key_name.each do |key, value|
+    if Exchange.find_by(currency: key)
+      rate = Exchange.find_by(currency: key)
+      rate.update(rate: value)
+    else
+      Exchange.create(currency: key, rate: value)
+    end
+  end
