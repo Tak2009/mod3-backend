2cc06d66bf90713a849b92945d3ba292e2bfa366
github username <github email address>
2019-10-21 12:35:35 +0100

    dealt with CORS

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -1,6 +1,6 @@
 class ExchangesController < ApplicationController
 
-# getExchnages
+# this is for displaying my data on broweser through my API. rails s and then go to http://localhost:3000/exchanges
     def index
         exchanges = Exchange.all
         render json: exchanges
