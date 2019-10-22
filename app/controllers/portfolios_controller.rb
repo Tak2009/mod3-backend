272943b1095795500ea015240f5e940e11f5e970
github username <github email address>
2019-10-22 07:17:50 +0100

    changed

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,12 +1,46 @@
 class PortfoliosController < ApplicationController
 
      # this is for displaying my data on broweser through my API. rails s and then go to http://localhost:3000/portfolios
-     def index
+    def index
         portfolios = Portfolio.all
-        render json: portfolios
+        # render json: portfolios
     end
-
+    
+    # get/portfolios/1
     def show
+        portfolio = Portfolio.find_by(id: params[:id])
         render json: portfolio
     end
+
+    # def new
+    #    portfolio = Portfolio.new
+    # end
+    
+    # post/portfolios
+    def create
+        portfolio = Portfolio.new(portfolio_params)
+        portfolio.save
+    end
+
+    # def edit
+	# 	portfolio = Portfolio.find(params[:id])
+	# end
+    
+    # patch/portfolios/1
+	def update
+        portfolio = Portfolio.find(params[:id])
+        portfolio.update(portfolio_params)
+    end
+    
+    # delete/portfolios/1
+    def destroy
+        Portfolio.destroy(params[:id])
+    end
+
+	private
+
+	def portfolio_params
+		params.require(:portfolio).permit!
+	end
+
 end
---
+++ app/serializers/portfolios_controller.rb
@@ -0,0 +1,5 @@
+class PortfoliosSerializer < ActiveModel::Serializer
+
+    attributes :id
+
+end
