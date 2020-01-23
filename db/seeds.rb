faef7f2e45c297c809b7de87d72d93062abc94cf
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-01-23 13:56:49 +0000

    importing fx logic changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,21 +9,21 @@ json = Net::HTTP.get(uri) #NET::HTTPを利用してAPIを呼ぶ
 result = JSON.parse(json) #返ってきたjsonデータをrubyの配列に変換するためのライン
 
 # 2.ここからはデータベースに取得したレートを保存するためのもの
-# result['quotes'].each do |key, value|
-#     if Exchange.find_by(currency: key)
-#       rate = Exchange.find_by(currency: key)
-#       rate.update(rate: value)
-#     else
-#       Exchange.create(currency: key, rate: value)
-#     end
-#   end
+result['quotes'].each do |key, value|
+    if Exchange.find_by(currency: key)
+      rate = Exchange.find_by(currency: key)
+      rate.update(rate: value)
+    else
+      Exchange.create(currency: key, rate: value)
+    end
+  end
 
-# 3.GBPベースでないので
+# 3.GBPベースでないので計算
  new_base_currency_gbp_rate = result['quotes']['USDGBP']
  result_base_currency_gbp = result['quotes'].map{|key,value| [key, (value/new_base_currency_gbp_rate).round(4)]}.to_h #GBPをGBPで割ってしまうためUSDGBPレートが１に塗り変わってしまうが先頭のUSDUSDがGBPの元になる
  result_base_currency_gbp_with_right_key_name = result_base_currency_gbp.map{|key,value| ["GBP" + key.slice(3,6), value]}.to_h # keynameの調整
  
-# 4.dbにセーブ
+# 4.3をdbにセーブ
  result_base_currency_gbp_with_right_key_name.each do |key, value|
     if Exchange.find_by(currency: key)
       rate = Exchange.find_by(currency: key)
@@ -33,8 +33,15 @@ result = JSON.parse(json) #返ってきたjsonデータをrubyの配列に変換
     end
   end
 
+# 不要なUSDを消去
+usd_base = Exchange.where("currency LIKE ?","USD%")
+usd_base.destroy_all
+
+
 c1 = Exchange.find(6)
 c2 = Exchange.find(5)
 
 p1 = Portfolio.create(:local_amt => 4000000, :home_amt => 0, :exchange_id => c1.id)
 p2 = Portfolio.create(:local_amt => 20000, :home_amt => 0, :exchange_id => c2.id)
+p1.update(:home_amt => (p1.local_amt/Exchange.find(p1.exchange_id).rate).round(3))
+p2.update(:home_amt => (p2.local_amt/Exchange.find(p2.exchange_id).rate).round(3))
