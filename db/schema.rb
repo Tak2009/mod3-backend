c96fc7f0f2579a1d69422b86375f31b4e4e1698d
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-03-02 10:38:28 +0000

    db

---
+++ db/schema.rb
@@ -0,0 +1,32 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_10_19_161234) do
+
+  create_table "exchanges", force: :cascade do |t|
+    t.string "currency"
+    t.float "rate"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.float "local_amt"
+    t.float "home_amt"
+    t.integer "exchange_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["exchange_id"], name: "index_portfolios_on_exchange_id"
+  end
+
+  add_foreign_key "portfolios", "exchanges"
+end
